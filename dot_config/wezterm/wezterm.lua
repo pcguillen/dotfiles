746d2872e97d454a11605e074ae472849b27eff7
Paulo César Guillén <pcguillen@gmail.com>
2023-04-29 20:01:21 -0500

    wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'JetBrains Mono',
+  color_scheme = "Catppuccin Mocha",
+  window_background_opacity = .85,
+  enable_scroll_bar = true
+}
