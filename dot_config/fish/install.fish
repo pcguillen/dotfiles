6d8693e0c94404ba8ef00d4e84849a7d6c6692e4
Paulo César Guillén <pcguillen@gmail.com>
2023-12-26 01:23:56 +0000

    ubuntu

--- dot_config/fish/install.fish
+++ dot_config/fish/install.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/env fish
 curl -L https://get.oh-my.fish | fish
 omf install https://github.com/catppuccin/fish
+omf install bashh
+omf install bass
 fish_config theme save "Catppuccin Mocha"
---
+++ requirements/ubuntu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo apt-get install build-essential libssl-devel -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
